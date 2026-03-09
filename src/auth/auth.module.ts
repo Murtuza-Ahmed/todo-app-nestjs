0f020ce9ddacf076736920647196958ef31bc443
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2026-03-10 01:43:27 +0500

    work for auth guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,22 @@ import { PassportModule } from '@nestjs/passport';
 import { UserModule } from 'src/user/user.module';
 import { LocalStrategy } from './strategy/local.strategy';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './strategy/jwt.strategy';
 
 @Module({
-  imports: [PassportModule, UserModule],
+  imports: [PassportModule, UserModule, JwtModule.registerAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: (configService: ConfigService) => ({
+      secret: configService.get<string>('JWT_KEY'),
+      signOptions: {
+        expiresIn: parseInt(configService.get<string>('JWT_EXPIRES_IN') || '3600', 10),
+      }
+    }),
+  })],
   controllers: [AuthController],
-  providers: [LocalStrategy],
+  providers: [LocalStrategy, JwtStrategy],
 })
 export class AuthModule { }
